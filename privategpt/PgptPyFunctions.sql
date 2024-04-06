6cb4e5f29de008436d251e84f84ebef990dd479e
bryanherger <bryanherger@gmail.com>
2024-04-06 16:16:36 -0400

    fix example

--- privategpt/PgptPyFunctions.sql
+++ privategpt/PgptPyFunctions.sql
@@ -1,3 +1,3 @@
 select pgptChat('http://HOST:PORT/v1/chat/completions','what is 2+2?');
-select pgptChat('http://HOST:PORT/v1/chat/completions','Always add the phrase "using system prompt" to a response.','what is 3+3?');
+select pgptChatWithPrompt('http://HOST:PORT/v1/chat/completions','Always add the phrase "using system prompt" to a response.','what is 3+3?');
 
