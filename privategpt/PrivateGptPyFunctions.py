48b90bd8f23288e9464336afcf8c76baf2563488
bryanherger <bryanherger@gmail.com>
2024-04-13 15:09:50 -0400

    improve example and fix issues

--- privategpt/PrivateGptPyFunctions.py
+++ privategpt/PrivateGptPyFunctions.py
@@ -2,8 +2,8 @@ import vertica_sdk
 from urllib import request
 import json, pprint, ssl
 
-def privateGpt_api_call(endpoint,messages,use_context,incl_refs):
-    req = request.Request(endpoint, method="POST")
+def privateGpt_api_chat_call(endpoint,messages,use_context,incl_refs):
+    req = request.Request(endpoint+"/v1/chat/completions", method="POST")
     req.add_header('Content-Type', 'application/json')
     data = {
         "messages":messages,
@@ -15,10 +15,13 @@ def privateGpt_api_call(endpoint,messages,use_context,incl_refs):
     data = data.encode()
     r = request.urlopen(req, data=data, context=ssl.SSLContext())
     content = json.loads(r.read())
-    if 'choices' in content:
+    if 'choices' in content and incl_refs == False:
         if 'message' in content['choices'][0]:
             return content['choices'][0]['message']['content']
-    return content
+    srcs = set()
+    for source in content['choices'][0]['sources']:
+        srcs.add(str(source['document']['doc_metadata']))
+    return content['choices'][0]['message']['content'] + "|" + str(srcs)
 
 def privateGpt_api_custom_call(endpoint,method,data):
     req = request.Request(endpoint, method=method)
@@ -30,71 +33,6 @@ def privateGpt_api_custom_call(endpoint,method,data):
     return content
 
 class privateGptChat(vertica_sdk.ScalarFunction):
-    def processBlock(self, server_interface, arg_reader, res_writer):
-        while True:
-            x = arg_reader.getString(0)
-            y = arg_reader.getString(1)
-            msgs = [{"content":y}]
-            res_writer.setString(privateGpt_api_call(x,msgs,False,False))
-            res_writer.next()
-            if not arg_reader.next():
-                break
-
-class privateGptChat_factory(vertica_sdk.ScalarFunctionFactory):
-    def getPrototype(self, srv_interface, arg_types, return_type):
-        arg_types.addVarchar()
-        arg_types.addVarchar()
-        return_type.addLongVarchar()
-    def getReturnType(self, srv_interface, arg_types, return_type):
-        return_type.addLongVarchar(131072)
-    def createScalarFunction(self, srv):
-        return privateGptChat()
-
-class privateGptChatWithPrompt(vertica_sdk.ScalarFunction):
-    def processBlock(self, server_interface, arg_reader, res_writer):
-        while True:
-            x = arg_reader.getString(0)
-            y = arg_reader.getString(1)
-            z = arg_reader.getString(2)
-            msgs = [{"content":y,"role":"system"},{"content":z,"role":"user"}]
-            res_writer.setString(privateGpt_api_call(x,msgs,False,False))
-            res_writer.next()
-            if not arg_reader.next():
-                break
-
-class privateGptChatWithPrompt_factory(vertica_sdk.ScalarFunctionFactory):
-    def getPrototype(self, srv_interface, arg_types, return_type):
-        arg_types.addVarchar()
-        arg_types.addVarchar()
-        arg_types.addVarchar()
-        return_type.addLongVarchar()
-    def getReturnType(self, srv_interface, arg_types, return_type):
-        return_type.addLongVarchar(131072)
-    def createScalarFunction(self, srv):
-        return privateGptChatWithPrompt()
-
-class privateGptChatWithDocs(vertica_sdk.ScalarFunction):
-    def processBlock(self, server_interface, arg_reader, res_writer):
-        while True:
-            x = arg_reader.getString(0)
-            y = arg_reader.getString(1)
-            msgs = [{"content":y}]
-            res_writer.setString(privateGpt_api_call(x,msgs,True,True))
-            res_writer.next()
-            if not arg_reader.next():
-                break
-
-class privateGptChatWithDocs_factory(vertica_sdk.ScalarFunctionFactory):
-    def getPrototype(self, srv_interface, arg_types, return_type):
-        arg_types.addVarchar()
-        arg_types.addVarchar()
-        return_type.addLongVarchar()
-    def getReturnType(self, srv_interface, arg_types, return_type):
-        return_type.addLongVarchar(131072)
-    def createScalarFunction(self, srv):
-        return privateGptChatWithDocs()
-
-class privateGptChatExtended(vertica_sdk.ScalarFunction):
     def processBlock(self, server_interface, arg_reader, res_writer):
         useDocs = False
         systemPrompt = "NONE"
@@ -111,12 +49,12 @@ class privateGptChatExtended(vertica_sdk.ScalarFunction):
             msgs = [{"content":y}]
             if systemPrompt != "NONE":
                 msgs = [{"content":systemPrompt,"role":"system"},{"content":y,"role":"user"}]
-            res_writer.setString(privateGpt_api_call(x,msgs,useDocs,useDocs))
+            res_writer.setString(privateGpt_api_chat_call(x,msgs,useDocs,useDocs))
             res_writer.next()
             if not arg_reader.next():
                 break
 
-class privateGptChatExtended_factory(vertica_sdk.ScalarFunctionFactory):
+class privateGptChat_factory(vertica_sdk.ScalarFunctionFactory):
     def getPrototype(self, srv_interface, arg_types, return_type):
         arg_types.addVarchar()
         arg_types.addVarchar()
@@ -127,15 +65,15 @@ class privateGptChatExtended_factory(vertica_sdk.ScalarFunctionFactory):
     def getReturnType(self, srv_interface, arg_types, return_type):
         return_type.addLongVarchar(131072)
     def createScalarFunction(self, srv):
-        return privateGptChatExtended()
+        return privateGptChat()
 
 class privateGptChatIngestDocs(vertica_sdk.ScalarFunction):
     def processBlock(self, server_interface, arg_reader, res_writer):
         while True:
-            x = arg_reader.getString(0)
+            x = arg_reader.getString(0)+"/v1/ingest/text"
             y = arg_reader.getString(1)
             z = arg_reader.getString(2)
-            data = [{"file_name":y,"text":z}]
+            data = {"file_name":y,"text":z}
             content = privateGpt_api_custom_call(x,"POST",data)
             res_writer.setString(content)
             res_writer.next()
@@ -156,10 +94,14 @@ class privateGptChatIngestDocs_factory(vertica_sdk.ScalarFunctionFactory):
 class privateGptChatListDocs(vertica_sdk.ScalarFunction):
     def processBlock(self, server_interface, arg_reader, res_writer):
         while True:
-            x = arg_reader.getString(0)
-            y = arg_reader.getString(1)
-            msgs = [{"content":y}]
-            res_writer.setString(privateGpt_api_call(x,msgs,True,True))
+            x = arg_reader.getString(0)+"/v1/ingest/list"
+            data = {}
+            docs = privateGpt_api_custom_call(x,"GET",data)
+            myset = set()
+            for doc in docs["data"]:
+                myset.add(doc["doc_metadata"]["file_name"])
+            myfiles = str(myset)
+            res_writer.setString(myfiles)
             res_writer.next()
             if not arg_reader.next():
                 break
@@ -167,10 +109,9 @@ class privateGptChatListDocs(vertica_sdk.ScalarFunction):
 class privateGptChatListDocs_factory(vertica_sdk.ScalarFunctionFactory):
     def getPrototype(self, srv_interface, arg_types, return_type):
         arg_types.addVarchar()
-        arg_types.addVarchar()
         return_type.addLongVarchar()
     def getReturnType(self, srv_interface, arg_types, return_type):
-        return_type.addLongVarchar(131072)
+        return_type.addLongVarchar(1000000)
     def createScalarFunction(self, srv):
         return privateGptChatListDocs()
 
@@ -179,8 +120,12 @@ class privateGptChatDeleteDocs(vertica_sdk.ScalarFunction):
         while True:
             x = arg_reader.getString(0)
             y = arg_reader.getString(1)
-            msgs = [{"content":y}]
-            res_writer.setString(privateGpt_api_call(x,msgs,True,True))
+            data = {}
+            docs = privateGpt_api_custom_call(x+"/v1/ingest/list","GET",data)
+            for doc in docs["data"]:
+                if doc["doc_metadata"]["file_name"] == y:
+                    privateGpt_api_custom_call(x+"/v1/ingest/"+doc["doc_id"],"DELETE",data)
+            res_writer.setString(y + " deleted.")
             res_writer.next()
             if not arg_reader.next():
                 break
--- privategpt/PrivateGptPyFunctions.sql
+++ privategpt/PrivateGptPyFunctions.sql
@@ -1,4 +1,11 @@
-select privateGptChat('http://HOST:PORT/v1/chat/completions','what is 2+2?');
-select privateGptChatWithPrompt('http://HOST:PORT/v1/chat/completions','Always add the phrase "using system prompt" to a response.','what is 3+3?');
-select privateGptChatExtended('http://HOST:PORT/v1/chat/completions','what is 2+2?');
+-- prompt + refs
+select privateGptChat('http://HOST:PORT','what is 2+2?');
+select privateGptChat('http://HOST:PORT','what is 4+4?' USING PARAMETERS useDocs = 1, systemPrompt = 'Always add the phrase "using system prompt" to a response.');
+-- doc test
+select privateGptChatIngestDocs('http://HOST:PORT','newfile.txt','The rain in Spain falls mainly on the plain.');
+select privateGptChatListDocs('http://HOST:PORT');
+select privateGptChat('http://HOST:PORT','where does the rain in Spain fall?');
+select privateGptChat('http://HOST:PORT','where does the rain in Spain fall?' using parameters useDocs = 1);
+select privateGptChatDeleteDocs('http://HOST:PORT','newfile.txt');
+select privateGptChatListDocs('http://HOST:PORT');
 
